70f242d6facd07019f61d3fe9368d09b8f71c9b4
user-no-18 <ece2023110@rcciit.org.in>
2026-04-06 07:51:25 +0530

    Configure for Vercel deployment and handle ephemeral SQLite

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,9 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.resolve(__dirname, '../../database.sqlite');
+const dbPath = process.env.VERCEL
+  ? path.join('/tmp', 'database.sqlite')
+  : path.resolve(__dirname, '../../database.sqlite');
 const db = new sqlite3.Database(dbPath);
 
 // promise wrappers so we don't have callback hell in controllers
